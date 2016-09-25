5ac8214307dfd249f3a4c0d25c9a964c35b9c744
yanghao <494608821@qq.com>
2016-09-25 20:57:29 +0800

    until step 8
    Signed-off-by: yanghao <494608821@qq.com>

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,20 +1,31 @@
 angular.module('phoneList').component('phoneList',{
 	templateUrl:'phone-list/phone-list.template.html',
-	controller: function PhoneListController() {
+	controller: ['$http',function PhoneListController($http) {
+		var self = this;
+		/*
 		this.phones = [
 			{
 			  name: 'Nexus S',
-			  snippet: 'Fast just got faster with Nexus S.'
+			  snippet: 'Fast just got faster with Nexus S.',
+			  age:3
 			}, {
 			  name: 'Motorola XOOM? with Wi-Fi',
-			  snippet: 'The Next, Next Generation tablet.'
+			  snippet: 'The Next, Next Generation tablet.',
+			  age:2
 			}, {
 			  name: 'MOTOROLA XOOM?',
-			  snippet: 'The Next, Next Generation tablet.'
+			  snippet: 'The Next, Next Generation tablet.',
+			  age:3
 			}, {
 			  name: 'MOTOROLA XOOM4',
-			  snippet: 'The Next, Next Generation tablet.'
+			  snippet: 'The Next, Next Generation tablet.',
+			  age:4
 			}
 		];
-	}
+		*/
+		this.orderProp = 'age';
+		$http.get('phones/phones.json').then(function(response) {
+			self.phones = response.data;
+		})
+	}]
 });
